1bf2597f505fae4a9c0490d7623e82e57f684664
Luis Flor Chacón <luisflorchacon7@gmail.com>
2024-11-26 17:09:52 +0000

    Fotos con object fit cover + rows de cada cart< segun el dispositivo

--- src/components/contentCard.jsx
+++ src/components/contentCard.jsx
@@ -1,17 +1,23 @@
-import Card from "react-bootstrap/Card";
-import Col from "react-bootstrap/Col";
+import React from "react";
 import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
 import Boton from "./botonsito";
 
 function Cartas() {
   return (
-    <Row xs={1} md={2} className="">
+    <Row className="d-flex flex-row justify-content-around">
       {Array.from({ length: 4 }).map((_, idx) => (
-        <Col key={idx}>
-          <Card>
-            <Card.Img variant="top" src="holder.js/100px160" />
+        <Col key={idx} xs={12} md={6} lg={3}>
+          <Card className="h-100">
+            <Card.Img
+              variant="top"
+              src="https://images.pexels.com/photos/29512123/pexels-photo-29512123/free-photo-of-villa-tropical-frente-al-mar-en-sri-lanka.jpeg?auto=compress&cs=tinysrgb&w=600"
+              alt={`Card ${idx + 1}`}
+              style={{objectFit: "cover"}}
+            />
             <Card.Body>
-              <Card.Title>Card title</Card.Title>
+              <Card.Title>Card {idx + 1}</Card.Title>
               <Card.Text>
                 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae
                 temporibus possimus laborum nobis qui rem nam saepe facilis
@@ -20,7 +26,7 @@ function Cartas() {
               </Card.Text>
             </Card.Body>
             <Card.Footer>
-                <Boton />
+              <Boton />
             </Card.Footer>
           </Card>
         </Col>
@@ -30,3 +36,4 @@ function Cartas() {
 }
 
 export default Cartas;
+
--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,11 +2,12 @@ import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
     
 function Footer() {
+  const currentYear = new Date().getFullYear();
   return (
     <Navbar className="bg-dark text-white" data-bs-theme="dark">
       <Container className="d-flex justify-content-center align-items-center">
         <p className="mb-0 small text-center">
-          Copyright © 2024. All rights reserved.
+          Copyright © {currentYear}. All rights reserved.
         </p>
       </Container>
     </Navbar>
